3f225586994133008ea43b3cd5b6b159481068e3
zac-roland <gilliland.zachary@knights.ucf.edu>
2019-07-13 08:45:00 -0400

    Server progress
    Begin implementation of general-use register and login

---
+++ backend/routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
+// Load user model
+const User = require("../../models/test-user.model");
+
--- backend/server.js
+++ backend/server.js
@@ -3,34 +3,34 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 8080;
+const localPORT = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 //Deployment version: connects to mLab//////////////////////////////////////////////////////////////////////////////
-// mongoose.connect('mongodb://heroku_9lgcjlsf:rhnbma5b96masjo0chuou34j2d@ds247827.mlab.com:47827/heroku_9lgcjlsf');
+const mongoURI = require("./config/keys").mongoURI;
+
+mongoose
+    .connect(
+        mongoURI, {useNewUrlParser: true}
+    )
+    .then( () => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || localPORT;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Testing version: connects to local database (if you have a local database at the declared location)//////////////
+// mongoose.connect('mongodb://127.0.0.1:27017/users', { useNewUrlParser: true });
 // const connection = mongoose.connection;
 
 // connection.once('open', function() {
 //     console.log("MogoDB connection established");
 // });
 
-// app.listen(processes.env.PORT || PORT, function() {
-//     console.log("Server running");
+// app.listen(PORT, function() {
+//     console.log("Server is running on port " + PORT);
 // });
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Testing version: connects to local database (if you have a local database at the declared location)//////////////
-mongoose.connect('mongodb://127.0.0.1:27017/users', { useNewUrlParser: true });
-const connection = mongoose.connection;
-
-connection.once('open', function() {
-    console.log("MogoDB connection established");
-});
-
-app.listen(PORT, function() {
-    console.log("Server is running on port " + PORT);
-});
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
