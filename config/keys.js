d1237b3ca7733c560a793686743ab265ffe5bec9
zac-roland <gilliland.zachary@knights.ucf.edu>
2019-07-14 17:23:32 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://<dbuser>:<dbpassword>@ds247827.mlab.com:47827/heroku_9lgcjlsf",
+  mongoURI: "mongodb://heroku_9lgcjlsf:rhnbma5b96masjo0chuou34j2d@ds247827.mlab.com:47827/heroku_9lgcjlsf",
   secretOrKey: "secret"
 };
