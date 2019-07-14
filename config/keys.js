2501fa1d2d1ee5fe7a4dc2a9aac0c7d588faa4c4
zac-roland <gilliland.zachary@knights.ucf.edu>
2019-07-14 13:46:59 -0400

    Push keys (this is not secure)

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: "mongodb://heroku_9lgcjlsf:rhnbma5b96masjo0chuou34j2d@ds247827.mlab.com:47827/heroku_9lgcjlsf",
+    secretOrKey: "secret",
+    PUBLISHABLE_KEY: "pk_test_5Bu3nJzw8njXk6aeH5LxKiW000fgRQd5i7",
+    SECRET_KEY: "sk_test_nfXNcIo4QQTs1tvISw1Sh2zh00EVkOwx5S"
+};
