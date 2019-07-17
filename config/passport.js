19fad4f8677ecee5a75f48a7f6b57ba0cc4b69d5
zac-roland <gilliland.zachary@knights.ucf.edu>
2019-07-17 18:39:40 -0400

    Fix indentions

--- config/passport.js
+++ config/passport.js
@@ -9,16 +9,16 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-  passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {
-      User.findById(jwt_payload.id)
-        .then(user => {
-          if (user) {
-            return done(null, user);
-          }
-          return done(null, false);
-        })
-        .catch(err => console.log(err));
-    })
-  );
+	passport.use(
+		new JwtStrategy(opts, (jwt_payload, done) => {
+			User.findById(jwt_payload.id)
+				.then(user => {
+					if (user) {
+						return done(null, user);
+					}
+					return done(null, false);
+				})
+				.catch(err => console.log(err));
+		})
+	);
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -16,94 +16,94 @@ const User = require("../../models/User");
 // @desc Register user
 // @access Public
 router.post("/register", (req, res) => {
-  // Form validation
+	// Form validation
 
-  const { errors, isValid } = validateRegisterInput(req.body);
+	const { errors, isValid } = validateRegisterInput(req.body);
 
-  // Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+	// Check validation
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
 
-  User.findOne({ email: req.body.email }).then(user => {
-    if (user) {
-      return res.status(400).json({ email: "Email already exists" });
-    } else {
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-      });
+	User.findOne({ email: req.body.email }).then(user => {
+		if (user) {
+			return res.status(400).json({ email: "Email already exists" });
+		} else {
+			const newUser = new User({
+				name: req.body.name,
+				email: req.body.email,
+				password: req.body.password
+			});
 
-      // Hash password before saving in database
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err));
-        });
-      });
-    }
-  });
+			// Hash password before saving in database
+			bcrypt.genSalt(10, (err, salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if (err) throw err;
+					newUser.password = hash;
+					newUser
+						.save()
+						.then(user => res.json(user))
+						.catch(err => console.log(err));
+				});
+			});
+		}
+	});
 });
 
 // @route POST api/users/login
 // @desc Login user and return JWT token
 // @access Public
 router.post("/login", (req, res) => {
-  // Form validation
+	// Form validation
 
-  const { errors, isValid } = validateLoginInput(req.body);
+	const { errors, isValid } = validateLoginInput(req.body);
 
-  // Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+	// Check validation
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
 
-  const email = req.body.email;
-  const password = req.body.password;
+	const email = req.body.email;
+	const password = req.body.password;
 
-  // Find user by email
-  User.findOne({ email }).then(user => {
-    // Check if user exists
-    if (!user) {
-      return res.status(404).json({ emailnotfound: "Email not found" });
-    }
+	// Find user by email
+	User.findOne({ email }).then(user => {
+		// Check if user exists
+		if (!user) {
+			return res.status(404).json({ emailnotfound: "Email not found" });
+		}
 
-    // Check password
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        // User matched
-        // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name
-        };
+		// Check password
+		bcrypt.compare(password, user.password).then(isMatch => {
+			if (isMatch) {
+				// User matched
+				// Create JWT Payload
+				const payload = {
+					id: user.id,
+					name: user.name
+				};
 
-        // Sign token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          {
-            expiresIn: 31556926 // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
-      } else {
-        return res
-          .status(400)
-          .json({ passwordincorrect: "Password incorrect" });
-      }
-    });
-  });
+				// Sign token
+				jwt.sign(
+					payload,
+					keys.secretOrKey,
+					{
+						expiresIn: 31556926 // 1 year in seconds
+					},
+					(err, token) => {
+						res.json({
+							success: true,
+							token: "Bearer " + token
+						});
+					}
+				);
+			} else {
+				return res
+					.status(400)
+					.json({ passwordincorrect: "Password incorrect" });
+			}
+		});
+	});
 });
 
 module.exports = router;
