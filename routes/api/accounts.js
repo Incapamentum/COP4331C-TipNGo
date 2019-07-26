1305af91df5c10c29134e608e0dc7600445e3abd
zac-roland <gilliland.zachary@knights.ucf.edu>
2019-07-26 07:53:50 -0400

    Clarify findtipper and findtippee

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const Tippee = require("../../models/Tippee");
 const Tipper = require("../../models/Tipper");
 
-// @route POST api/accounts/tipper
+// @route POST api/accounts/findtipper
 // @desc Request tipper document by id
-router.post("/tipper", (req, res) => {
-    const _id = req.body.id;
+router.post("/findtipper", (req, res) => {
+    const userid = req.body.id;
 
-    Tipper.findOne({ _id }).then(tipper => {
+    Tipper.findOne({ userid }).then(tipper => {
         if (!tipper) {
             return res.status(404).json({ tippernotfound: "Tipper not found" });
         }
@@ -18,12 +18,13 @@ router.post("/tipper", (req, res) => {
     })
 });
 
-// @route POST api/accounts/tippee
+// @route POST api/accounts/findtippee
 // @desc Request tippee document by id
-router.post("/tippee", (req, res) => {
-    const _id = req.body.id;
+router.post("/findtippee", (req, res) => {
+    const userid = req.body.id;
+    
 
-    Tippee.findOne({ _id }).then(tippee => {
+    Tippee.findOne({ userid }).then(tippee => {
         if (!tippee) {
             return res.status(404).json({ tippeenotfound: "Tippee not found" });
         }
